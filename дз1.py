035a16f20102d89d3790683a57b914c76fd5b893
LilChich4 <cfyg456@mail.ru>
2024-08-22 02:18:14 +0300

    Add files via upload

---
+++ дз1.py
@@ -0,0 +1,10 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2st program
+print(9.99>9.98 and 1000 != 1000.1)
+# 3st program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+# 4st program
+print(int(10 *float('123.456')-1230))
